f8da5578ce504dd5b6bdb0657f1f27b80c522617
Tiago Censi <tiagocensi132@gmail.com>
2025-05-06 10:51:32 -0300

    login

--- PágI.js
+++ PágI.js
@@ -17,6 +17,28 @@ if (tipo === "coordenador" || tipo === "bolsista"){
     document.getElementById("btn-cadastrar-livro").style.display = "block";
 }
 
+document.addEventListener('DOMContentLoaded', () => {
+    const tipo = localStorage.getItem("tipoUsuario");
+    const logoutBtn = document.querySelector(".logout-link");
 
+    if (tipo === "coordenador") {
+        document.getElementById("btn-cadastrar-bolsista").style.display = "block";
+        document.getElementById("btn-cadastrar-livro").style.display = "block";
+        logoutBtn.style.display = "inline-block";
+    } else if (tipo === "bolsista") {
+        document.getElementById("btn-cadastrar-livro").style.display = "block";
+        logoutBtn.style.display = "inline-block";
+    } else {
+        // Se não for nenhum dos dois, esconde tudo
+        document.getElementById("btn-cadastrar-livro").style.display = "none";
+        document.getElementById("btn-cadastrar-bolsista").style.display = "none";
+        logoutBtn.style.display = "none";
+    }
+});
 
+function logout(){
+    localStorage.removeItem('usuarioLogado');
+    localStorage.removeItem('tipoUsuario');
+    window.location.href = 'PáginaInicial.html';
+}
 
