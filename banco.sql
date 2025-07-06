8ffdc2044d7ff746a10e4dcec18c332dfc86c035
Tiago Censi <tiagocensi132@gmail.com>
2025-07-06 10:06:58 -0300

    alt

--- banco.sql
+++ banco.sql
@@ -29,7 +29,7 @@ CREATE TABLE bolsistas (
 );
 
 CREATE TABLE coordenadores (
-    idcornedadores INT AUTO_INCREMENT PRIMARY KEY,
+    idcoordenador INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100)  UNIQUE NOT NULL,
     senha VARCHAR(100) NOT NULL
